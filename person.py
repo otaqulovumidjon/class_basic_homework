beec29cc3dd793e9e4f395deca6d26ad8355d8b0
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-05-03 09:59:56 +0500

    x7

--- person.py
+++ person.py
@@ -1,1 +1,10 @@
 #Create a "Person" class
+
+class Person:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+p0 = Person("Umidjon", "Otaqulov", 19)
+print(p0.first_name)
